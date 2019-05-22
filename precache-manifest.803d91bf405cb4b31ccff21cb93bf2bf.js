51c054b61e043f7cc56c9e19f9e31ce0cfd0b113
your <chameeradulanga87@gmail.com>
2019-05-23 00:10:25 +0530

    Updates

---
+++ precache-manifest.803d91bf405cb4b31ccff21cb93bf2bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "8aa1e0bebf317502fb1f",
+    "url": "/portfolio/static/js/main.d3b034e6.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8aa1e0bebf317502fb1f",
+    "url": "/portfolio/static/css/main.31acbb5c.chunk.css"
+  },
+  {
+    "revision": "e3cbd673846773c6bdd2edf15cb90f23",
+    "url": "/portfolio/index.html"
+  }
+];
