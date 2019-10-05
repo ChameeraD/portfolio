87140ce9aeed61a41488ce1e09a7faaa71ef0e35
your <chameeradulanga87@gmail.com>
2019-10-05 22:00:00 +0530

    timeline update

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,106 +1,175 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class skills extends Component {
   render() {
     return (
       <div>
-      <section className="colorlib-skills" data-section="skills">
-				<div className="colorlib-narrow-content">
-					<div className="row">
-						<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-							<span className="heading-meta">My Specialty</span>
-							<h2 className="colorlib-heading animate-box">My Skills</h2>
-						</div>
-					</div>
-					<div className="row">
-						{/*<div className="col-md-12 animate-box" data-animate-effect="fadeInLeft">
+        <section className="colorlib-skills" data-section="skills">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <span className="heading-meta">My Specialty</span>
+                <h2 className="colorlib-heading animate-box">My Skills</h2>
+              </div>
+            </div>
+            <div className="row">
+              {/*<div className="col-md-12 animate-box" data-animate-effect="fadeInLeft">
 							<p>I guess this is the phase where we are shine , right :)</p>
 						</div>*/}
-            <h2 className="colorlib-heading animate-box"> Web Development </h2>
-						<div className="col-md-6 animate-box" data-animate-effect="fadeInLeft">
-							<div className="progress-wrap">
-								<h3>MEAN Stack</h3>
-								<div className="progress">
-								 	<div className="progress-bar color-1" role="progressbar" aria-valuenow="80"
-								  	aria-valuemin="0" aria-valuemax="100" style= {{ width :"80%" }}> 
-								    <span>80%</span>
-								  	</div>
-								</div>
-							</div>
-						</div>
-						<div className="col-md-6 animate-box" data-animate-effect="fadeInRight">
-							<div className="progress-wrap">
-								<h3>ASP .NET Core</h3>
-								<div className="progress">
-								 	<div className="progress-bar color-2" role="progressbar" aria-valuenow="50"
-								  	aria-valuemin="0" aria-valuemax="100" style= {{width:"50%"}}>
-								    <span>50%</span>
-								  	</div>
-								</div>
-							</div>
-						</div>
+              <h2 className="colorlib-heading animate-box">
+                {" "}
+                Web Development{" "}
+              </h2>
+              <div
+                className="col-md-6 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <div className="progress-wrap">
+                  <h3>MEAN Stack</h3>
+                  <div className="progress">
+                    <div
+                      className="progress-bar color-1"
+                      role="progressbar"
+                      aria-valuenow=""
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: "100%" }}
+                    >
+                      <span></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-6 animate-box"
+                data-animate-effect="fadeInRight"
+              >
+                <div className="progress-wrap">
+                  <h3>ASP .NET Core</h3>
+                  <div className="progress">
+                    <div
+                      className="progress-bar color-2"
+                      role="progressbar"
+                      aria-valuenow=""
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: "100%" }}
+                    >
+                      <span></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
-						<div className="col-md-6 animate-box" data-animate-effect="fadeInLeft">
-							<div className="progress-wrap">
-								<h3>HTML5 , CSS3 & Bootstrap</h3>
-								<div className="progress">
-								 	<div className="progress-bar color-3" role="progressbar" aria-valuenow="80"
-								  	aria-valuemin="0" aria-valuemax="100" style= {{ width:"80%" }}>
-								    <span>80%</span>
-								  	</div>
-								</div>
-							</div>
-						</div>
-						<div className="col-md-6 animate-box" data-animate-effect="fadeInRight">
-							<div className="progress-wrap">
-								<h3>Node JS</h3>
-								<div className="progress">
-								 	<div className="progress-bar color-4" role="progressbar" aria-valuenow="75"
-								  	aria-valuemin="0" aria-valuemax="100" style={{ width:"75%"}}>
-								    <span>75%</span>
-								  	</div>
-								</div>
-							</div>
-						</div>
+              <div
+                className="col-md-6 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <div className="progress-wrap">
+                  <h3>HTML5 , CSS3 & Bootstrap</h3>
+                  <div className="progress">
+                    <div
+                      className="progress-bar color-3"
+                      role="progressbar"
+                      aria-valuenow=""
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: "100%" }}
+                    >
+                      <span></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-6 animate-box"
+                data-animate-effect="fadeInRight"
+              >
+                <div className="progress-wrap">
+                  <h3>Node JS</h3>
+                  <div className="progress">
+                    <div
+                      className="progress-bar color-4"
+                      role="progressbar"
+                      aria-valuenow=""
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: "100%" }}
+                    >
+                      <span></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
-            <h2 className="colorlib-heading animate-box"> Databases</h2>
-							<div className="col-md-6 animate-box" data-animate-effect="fadeInLeft">
-								<div className="progress-wrap">
-									<h3>MY SQL</h3>
-									<div className="progress">
-										<div className="progress-bar color-5" role="progressbar" aria-valuenow="90"
-											aria-valuemin="0" aria-valuemax="100" style={{ width: "90%" }}>
-											<span>90%</span>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-6 animate-box" data-animate-effect="fadeInRight">
-								<div className="progress-wrap">
-									<h3>MS SQL</h3>
-									<div className="progress">
-										<div className="progress-bar color-6" role="progressbar" aria-valuenow="85"
-											aria-valuemin="0" aria-valuemax="100" style={{ width: "85%" }}>
-											<span>85%</span>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-6 animate-box" data-animate-effect="fadeInLeft">
-								<div className="progress-wrap">
-									<h3>Mongo DB</h3>
-									<div className="progress">
-										<div className="progress-bar color-7" role="progressbar" aria-valuenow="70"
-											aria-valuemin="0" aria-valuemax="100" style={{ width: "70%" }}>
-											<span>70%</span>
-										</div>
-									</div>
-								</div>
-							</div>
-					</div>
-				</div>
-			</section>
+              <h2 className="colorlib-heading animate-box"> Databases</h2>
+              <div
+                className="col-md-6 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <div className="progress-wrap">
+                  <h3>MY SQL</h3>
+                  <div className="progress">
+                    <div
+                      className="progress-bar color-5"
+                      role="progressbar"
+                      aria-valuenow=""
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: "100%" }}
+                    >
+                      <span></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-6 animate-box"
+                data-animate-effect="fadeInRight"
+              >
+                <div className="progress-wrap">
+                  <h3>MS SQL</h3>
+                  <div className="progress">
+                    <div
+                      className="progress-bar color-6"
+                      role="progressbar"
+                      aria-valuenow=""
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: "100%" }}
+                    >
+                      <span></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-6 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <div className="progress-wrap">
+                  <h3>Mongo DB</h3>
+                  <div className="progress">
+                    <div
+                      className="progress-bar color-7"
+                      role="progressbar"
+                      aria-valuenow=""
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: "100%" }}
+                    >
+                      <span></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-    )
+    );
   }
 }
--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -16,24 +16,35 @@ export default class Timeline extends Component {
             <div className="row">
               <div className="col-md-12">
                 <div className="timeline-centered">
-                  {/* <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                   <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
                     <div className="timeline-entry-inner">
                       <div className="timeline-icon color-3">
                         <i className="fas fa-pen" />
                       </div>
                       <div className="timeline-label">
-                        <h2><a href="#">Internship at 99xTechnologies</a> <span>2019-present</span></h2>
-                        <p>I recently have joined the Juniper Networs as a Software Developer-Intern in the DevOps team. My major part of the work has been into the field of creating sustainable and flexible CI/CD (Continuous Integration & Continuous Development) pipelines which enables developers at Juniper to do rapid development. I am also exploring the role of container technology like Docker and container-orchrastation technologies like K8s, Swarm to create industry standard production environments.</p>
+                        <h2><a href="#">99x Technologies - Trainee Software Engineer</a> <span>2019 Aug - present</span></h2>
+                        <p>Currently I work as a trainee software engineer in 99x Technologies.</p>
                       </div>
                     </div>
-                  </article> */}
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-2">
+                        <i className="fas fa-pen" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2><a href="#">Telexar - Trainee Software Engineer</a> <span>2019 Feb - 2019 Aug</span></h2>
+                        <p>I have worked in Telexar Software as a developer for 6 month period.</p>
+                      </div>
+                    </div>
+                  </article>
                   <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
                     <div className="timeline-entry-inner">
                       <div className="timeline-icon color-4">
                       <i className="fas fa-pen" />
                       </div>
                       <div className="timeline-label">
-                        <h2><a href="#">Undergraduate at University Of Moratuwa</a> <span>2015-Present</span></h2>
+                        <h2><a href="#">Undergraduate at University Of Moratuwa</a> <span>2015 - Present</span></h2>
                         <p>I'm an undergraduate in Unversity of Moratuwa Faculty of Information Technology. Java, DBMS, Data structures & Algorithms, Networking, Web development,AI, Markating are some key areas of this degree and currently I have a GPA of 3.8</p>
                       </div>
                     </div>
@@ -44,7 +55,7 @@ export default class Timeline extends Component {
                       <i className="fas fa-pen" />
                       </div>
                       <div className="timeline-label">
-                        <h2><a href="#">Secondary and Higher Education</a> <span>2006-2015</span></h2>
+                        <h2><a href="#">Secondary and Higher Education</a> <span>2006 - 2015</span></h2>
                         <p>I'm a proud Richmondite with 8A s and 1 B for O/L and A B B for A/L from physical science stream.</p> 
                       </div>
                     </div>
